97e4bfe21ef99aec46a127312375ac01e624b43d
Christopher Melo-Pelaez <102987797+chrism3lo@users.noreply.github.com>
2022-05-03 16:28:21 -0400

    Update app.js

--- app.js
+++ app.js
@@ -141,3 +141,30 @@ function gcd_two_numbers(x, y) {
   jim.driveToWork(50)
   jim.driveAroundTheWorld(20000)
   jim.runErrands(15)
+
+
+// Hard 1
+
+let isParenthesisMatching = str => {
+  let stack = [];
+  let open = {
+    '{': '}',
+    '[': ']',
+    '(': ')'
+  };
+  let closed = {
+    '}': true,
+    ']': true,
+    ')': true
+  };
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (open[char]) {
+      stack.push(char);
+    } else if (closed[char]) {
+      if (open[stack.pop()] !== char) return false;
+    }
+  }
+  return stack.length === 0;
+};
+console.log(isParenthesisMatching('(Hello)'));
