d05fb53687a7db27e4d1bfc1198dd61d21c34321
Christopher Melo-Pelaez <christophermelo-pelaez@MacBook-Pro-2.attlocal.net>
2022-05-03 14:28:26 -0400

    Medium 3

--- app.js
+++ app.js
@@ -52,19 +52,67 @@ console.log(allNums.filter(isPrime))
 //Create a Function Called “vowelChecker” that takes in 
 //a single argument (x) and logs weather or not the input is a vowel
 
-let userInput = prompt("Enter a letter to see if it is a vowel.").toUpperCase();
+// let userInput = prompt("Enter a letter to see if it is a vowel.").toUpperCase();
 
 
-function isVowel(x) {
-    var result;
-    result = x == "A" || x == "E" || x == "I" || x == "O" || x == "U";
-    if (result === true){
-        console.log("this is a vow")
-    } else{
-        console.log("this is not a vowel.")
+// function isVowel(x) {
+//     var result;
+//     result = x == "A" || x == "E" || x == "I" || x == "O" || x == "U";
+//     if (result === true){
+//         console.log("this is a vow")
+//     } else{
+//         console.log("this is not a vowel.")
+//     }
+//     return result;
+// }
+// isVowel(userInput)
+
+
+//Medium 2
+
+function checkStringsAnagram(a, b) {
+    let len1 = a.length;
+    let len2 = b.length;
+    if(len1 !== len2){
+       console.log('Invalid Input');
+       return
     }
-    return result;
-}
-isVowel(userInput)
+    let str1 = a.split('').sort().join('');
+    let str2 = b.split('').sort().join('');
+    if(str1 === str2){
+       console.log("It is an anagam");
+    } else { 
+       console.log("It is not an anagram");
+    }
+ }
+ checkStringsAnagram("code 10 cohort","cohort code 10")
 
+ //Medium 3
+// let hcf;
+// function gcd(num1,num2){
+//     for (let i = 1; i <= num1 && i <= num2; i++){
+//         if (num1 % i === 0 && num2 % i === 0){
+//              return hcf = i;
+//         }
+//     }
+// }
+// console.log(gcd(60,72));
 
+function gcd_two_numbers(x, y) {
+    if ((typeof x !== 'number') || (typeof y !== 'number'))
+      return false;
+    x = Math.abs(x);
+    y = Math.abs(y);
+    while(y) {
+      var t = y;
+      y = x % y;
+      x = t;
+    }
+    return x;
+  }
+
+  console.log(gcd_two_numbers(72,60));
+
+  // Hard 1
+
+  
